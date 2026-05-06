d3914583ab03e1be215f741f297dacf781773b8a
bukamal <manhal.almasriiii199119@gmail.com>
2026-05-06 14:05:12 +0300

    gdd

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alrajhi-offline-v4';
+const CACHE_NAME = 'alrajhi-offline-v5';
 const BASE_PATH = self.location.pathname.replace(/service-worker\.js$/, '');
 
 const urlsToCache = [
